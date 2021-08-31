a18a5cb82fc079703c990872343adfead2e30d05
unknown <vladancupric@gmail.com>
2021-08-31 22:44:47 +0200

    Add bootstrap 4 in react file, add loading page. simple folder i files

---
+++ marvel-heroes/src/components/Footer/Footer.js
@@ -0,0 +1,16 @@
+import React from "react";
+import {Navbar} from "react-bootstrap"
+import './Footer.css';
+
+const Footer = () => {
+    return (
+        <Navbar className={"footer justify-content-center"}>
+            <Navbar.Brand>
+                <span className={"name"}>&copy; 2021, by Sasa Andjelkovic, Nikola Colovic and Vladan Cupric</span>
+            </Navbar.Brand>
+
+        </Navbar>
+      );
+}
+ 
+export default Footer;
